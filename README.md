77b34fef6bce0d6ce735ea5a9bca2e50d8fbec5c
Shooty215 <b.heinr@web.de>
2021-05-12 06:55:12 +0200

    Changes: Update readme file to give a better understanding of the project, how it is meant to be used and what its current status is. Bytes are now saved to idividually named images and not to preset file. Added crude validations to code. Added contours of future inputs to main method.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # dictPicSubscriber
+## Description:
 Subscribes to broker's topic, saves picture and gives impulse to ring alert.
 
 Jar is meant to run in terminal with connection and authentication credentials as arguments.
+## Planned Usage:
+jar dirPicSubscriber.jar <broker_ip> <broker_port> <broker_topic> <image_save_directory> <key_store_directory> <user_name> <user_password>
+
+## Status:
+Running project's main method will subscribe to preset broker, saving received files to a preset file.
 
 Project is still in its infancy.
 
--- src/main/java/com/mqtt/client/paho/subscriber/App.java
+++ src/main/java/com/mqtt/client/paho/subscriber/App.java
@@ -10,23 +10,28 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.io.ByteArrayInputStream;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.imageio.ImageIO;
 
 public final class App {
+
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     *
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         /*
-         * String brokerParameter = args[0]; String channelParameter = args[1];
+         * jar dirPicSubscriber.jar <broker_ip> <broker_port> <broker_topic>
+         * <image_save_directory> <key_store_directory> <user_name> <user_password>
          */
         String brokerParameter = "tcp://localhost:1883";
+        // String portParameter = "1883";
         String channelParameter = "test";
+        String imageDirectory = "test";
+        // String keyStoreDirectory = "test";
+        // String userName = "test";
+        // String userPassword = "test";
 
         try {
 
@@ -36,21 +41,49 @@ public final class App {
 
                 @Override
                 public void connectionLost(Throwable throwable) {
+
+                    System.out.println("Connection lost!");
+
                 }
 
                 @Override
                 public void messageArrived(String t, MqttMessage m) throws Exception {
 
-                    System.out.println(new String(m.getPayload()));
+                    System.out.println("Message received! Assessing bytes..");
 
-                    byte[] data = m.getPayload();
-                    ByteArrayInputStream bis = new ByteArrayInputStream(data);
-                    BufferedImage bImage2 = ImageIO.read(bis);
-                    File file = new File("/home/sht/output.jpg");
-                    file.getParentFile().mkdirs();
-                    file.createNewFile();
-                    ImageIO.write(bImage2, "jpg", file);
-                    System.out.println("image created");
+                    // tries to save image from received bytes
+                    try {
+
+                        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+                        LocalDateTime currentTime = LocalDateTime.now();
+
+                        String fileName = dateTimeFormatter.format(currentTime) + ".jpeg";
+
+                        // String filePath = imageDirectory + fileName;
+
+                        byte[] data = m.getPayload();
+
+                        ByteArrayInputStream bis = new ByteArrayInputStream(data);
+
+                        BufferedImage bImage2 = ImageIO.read(bis);
+
+                        // File file = new File(filePath);
+                        File file = new File("/home/sht/" + fileName);
+
+                        file.getParentFile().mkdirs();
+
+                        file.createNewFile();
+
+                        ImageIO.write(bImage2, "jpeg", file);
+
+                        System.out.println("Image created!");
+
+                    } catch (Exception e) {
+
+                        System.out.println("Could not create image file from bytes!");
+
+                    }
 
                 }
 
@@ -62,9 +95,11 @@ public final class App {
                 }
 
             });
+
             MqttConnectOptions options = new MqttConnectOptions();
 
-            // options.setWill("test", "Disconnect".getBytes(), 2, true);
+            // best to use will on a different topic to avoid intermingling with images'
+            // bytes. options.setWill("test", "Disconnect".getBytes(), 2, true);
 
             options.setUserName("userName");
 
