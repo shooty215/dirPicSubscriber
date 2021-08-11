fa5eae263693844176db6d2aa0ba9e3d1626f67b
Shooty215 <b.heinr@web.de>
2021-08-11 05:27:43 +0200

    -added socket factory to code -first atempted to encrypt client-server-connection -INTEGRAL TESTING

--- src/main/java/com/mqtt/client/paho/subscriber/App.java
+++ src/main/java/com/mqtt/client/paho/subscriber/App.java
@@ -1,19 +1,39 @@
 package com.mqtt.client.paho.subscriber;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.imageio.ImageIO;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.bouncycastle.openssl.PEMDecryptorProvider;
+import org.bouncycastle.openssl.PEMEncryptedKeyPair;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
+import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-
-import java.io.ByteArrayInputStream;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import javax.imageio.ImageIO;
 
 public final class App {
 
@@ -24,6 +44,7 @@ public final class App {
         /*
          * jar dirPicSubscriber.jar <broker_ip> <broker_port> <broker_topic>
          * <image_save_directory> <key_store_directory> <user_name> <user_password>
+         * <ca_password>
          */
 
         // Inputs for meant use.
@@ -34,19 +55,11 @@ public final class App {
         String keyStoreParameter = args[4];
         String userNameParameter = args[5];
         String userPasswordParameter = args[6];
-
-        // Test inputs.
-        // String brokerParameter = "localhost";
-        // String portParameter = "1883";
-        // String channelParameter = "test";
-        // String imageDirectory = "/home/sht/dirPic/testImageSaveDirectory/";
-        // String keyStoreDirectory = "test";
-        // String userName = "test";
-        // String userPassword = "test";
+        String caPassword = args[7];
 
         try {
 
-            String brokerDetails = "tcp://" + brokerParameter + ":" + portParameter;
+            String brokerDetails = "ssl://" + brokerParameter + ":" + portParameter;
 
             MqttClient client = new MqttClient(brokerDetails, "idSubscriber" + MqttClient.generateClientId());
 
@@ -110,6 +123,11 @@ public final class App {
 
             MqttConnectOptions options = new MqttConnectOptions();
 
+            SSLSocketFactory socketFactory = getSocketFactory(keyStoreParameter + "ca_cert.pem",
+                    keyStoreParameter + "client_crt.pem", keyStoreParameter + "client_key.pem", caPassword);
+
+            options.setSocketFactory(socketFactory);
+
             // best to use will on a different topic to avoid intermingling with images'
             // bytes. options.setWill("test", "Disconnect".getBytes(), 2, true);
 
@@ -119,7 +137,13 @@ public final class App {
 
             options.setCleanSession(false);
 
+            System.out.println("Connecting...");
+
             client.connect(options);
+
+            System.out.println("Connected!");
+
+            System.out.println("Subscribing...");
 
             client.subscribe(channelParameter);
 
@@ -129,8 +153,76 @@ public final class App {
 
         } catch (MqttException e) {
 
-            System.out.println("Not Subscribed!");
+            System.out.println("Not Subscribed! - Server Error.");
+
+        } catch (Exception e) {
+
+            System.out.println("Not Subscribed! - Something Went Wrong.");
 
         }
     }
+
+    private static SSLSocketFactory getSocketFactory(final String caCrtFile, final String crtFile, final String keyFile,
+            final String password) throws Exception {
+        Security.addProvider(new BouncyCastlePQCProvider());
+
+        // load CA certificate
+        X509Certificate caCert = null;
+
+        FileInputStream fis = new FileInputStream(caCrtFile);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        while (bis.available() > 0) {
+            caCert = (X509Certificate) cf.generateCertificate(bis);
+            // System.out.println(caCert.toString());
+        }
+
+        // load client certificate
+        bis = new BufferedInputStream(new FileInputStream(crtFile));
+        X509Certificate cert = null;
+        while (bis.available() > 0) {
+            cert = (X509Certificate) cf.generateCertificate(bis);
+            // System.out.println(caCert.toString());
+        }
+
+        // load client private key
+        PEMParser pemParser = new PEMParser(new FileReader(keyFile));
+        Object object = pemParser.readObject();
+        PEMDecryptorProvider decProv = new JcePEMDecryptorProviderBuilder().build(password.toCharArray());
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+        KeyPair key;
+        if (object instanceof PEMEncryptedKeyPair) {
+            System.out.println("Encrypted key - we will use provided password");
+            key = converter.getKeyPair(((PEMEncryptedKeyPair) object).decryptKeyPair(decProv));
+        } else {
+            System.out.println("Unencrypted key - no password needed");
+            key = converter.getKeyPair((PEMKeyPair) object);
+        }
+        pemParser.close();
+
+        // CA certificate is used to authenticate server
+        KeyStore caKs = KeyStore.getInstance(KeyStore.getDefaultType());
+        caKs.load(null, null);
+        caKs.setCertificateEntry("ca-certificate", caCert);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+        tmf.init(caKs);
+
+        // client key and certificates are sent to server so it can authenticate
+        // us
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(null, null);
+        ks.setCertificateEntry("certificate", cert);
+        ks.setKeyEntry("private-key", key.getPrivate(), password.toCharArray(),
+                new java.security.cert.Certificate[] { cert });
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(ks, password.toCharArray());
+
+        // finally, create SSL socket factory
+        SSLContext context = SSLContext.getInstance("TLSv1.2");
+        context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+        return context.getSocketFactory();
+    }
+
 }
